53d7304d001c77a49f61ebcc33a1e0cf5a5eddca
Rostyslav Bryzgunov <kottenator@gmail.com>
2017-01-15 22:32:34 -0500

    More tests for args validation

--- test/test_cli.js
+++ test/test_cli.js
@@ -33,108 +33,136 @@ describe('CLI', function() {
   // expect tests to be slow, due to nature of the CLI (invoking sub-processes)
   this.slow(500);
 
-  before(function() {
-    if (!fs.existsSync('test/build/')) {
-      fs.mkdirSync('test/build/');
-    }
+  describe('Arguments validation', function() {
+    testCommand(
+      clientPath,
+      "show help message if there are no arguments",
+      function(err, stdout, stderr) {
+        checkHelpMessage(stderr);
+      }
+    );
+
+    testCommand(
+      clientPath + ' --help',
+      "show help message if there is '--help' argument",
+      function(err, stdout) {
+        checkHelpMessage(stdout);
+      }
+    );
+
+    testCommand(
+      clientPath + ' -v -h',
+      "show help message if there is '-h' argument, even if args are invalid",
+      function(err, stdout) {
+        checkHelpMessage(stdout);
+      }
+    );
+
+    testCommand(
+      clientPath + ' --version',
+      "show version if there is '--version' argument",
+      function(err, stdout) {
+        assert.equal(stdout, version + '\n');
+      }
+    );
+
+    testCommand(
+      clientPath + ' -v -V',
+      "show version if there is '-V' argument, even if args are invalid",
+      function(err, stdout) {
+        assert.equal(stdout, version + '\n');
+      }
+    );
+
+    testCommand(
+      clientPath + ' -v',
+      "expect at least one input path",
+      function(err, stdout, stderr) {
+        assert.equal(stderr, Client.prototype.messages.NO_INPUT_PATH + '\n');
+      }
+    );
+
+    testCommand(
+      clientPath + ' input-1.scss input-2.scss',
+      "expect no more than one input path",
+      function(err, stdout, stderr) {
+        assert.equal(stderr, Client.prototype.messages.EXTRA_POS_ARGS + '\n');
+      }
+    );
+
+    testCommand(
+      clientPath + ' input.scss -o output-1.css -o output-2.css',
+      "expect no more than one output path",
+      function(err, stdout, stderr) {
+        assert.equal(stderr, Client.prototype.messages.EXTRA_OUTPUT_PATH + '\n');
+      }
+    );
+
+    testCommand(
+      clientPath + ' input.scss -r one/ -r two/',
+      "expect no more than one root dir",
+      function(err, stdout, stderr) {
+        assert.equal(stderr, Client.prototype.messages.EXTRA_ROOT_DIR + '\n');
+      }
+    );
+
+    testCommand(
+      clientPath + ' input.scss -c "grep A" -c "grep B"',
+      "expect no more than one command",
+      function(err, stdout, stderr) {
+        assert.equal(stderr, Client.prototype.messages.EXTRA_COMMAND + '\n');
+      }
+    );
   });
 
-  testCommand(
-    clientPath,
-    "shows help message if there are no arguments",
-    function(err, stdout, stderr) {
-      checkHelpMessage(stderr);
-    }
-  );
-
-  testCommand(
-    clientPath + ' --help',
-    "shows help message if there is '--help' argument",
-    function(err, stdout) {
-      checkHelpMessage(stdout);
-    }
-  );
-
-  testCommand(
-    clientPath + ' -v -h',
-    "shows help message if there is '-h' argument, even if args are invalid",
-    function(err, stdout) {
-      checkHelpMessage(stdout);
-    }
-  );
-
-  testCommand(
-    clientPath + ' --version',
-    "shows version if there is '--version' argument",
-    function(err, stdout) {
-      assert.equal(stdout, version + '\n');
-    }
-  );
-
-  testCommand(
-    clientPath + ' -v -V',
-    "shows version if there is '-V' argument, even if args are invalid",
-    function(err, stdout) {
-      assert.equal(stdout, version + '\n');
-    }
-  );
-
-  testCommand(
-    clientPath + ' -v',
-    "expects at least one input path",
-    function(err, stdout, stderr) {
-      assert.equal(stderr, Client.prototype.messages.NO_INPUT_PATH + '\n');
-    }
-  );
-
-  testCommand(
-    clientPath + ' input-1.scss input-2.scss',
-    "expects no more than one input path",
-    function(err, stdout, stderr) {
-      assert.equal(stderr, Client.prototype.messages.EXTRA_POS_ARGS + '\n');
-    }
-  );
-
-  (function() {
-    var inputPath = 'test/resources/simple.scss';
-
-    testCommandAsync(
-      clientPath + ' ' + inputPath,
-      "outputs contents of the input file to stdout if there's no '-o' option",
-      function(subprocess, done) {
-        // file content should appear in the stdout
-        subprocess.stdout.on('data', function(data) {
-          assert.equal(data.toString(), fs.readFileSync(inputPath).toString());
-          subprocess.kill();
-          done();
-        });
+  describe('Run', function() {
+    before(function() {
+      if (!fs.existsSync('test/build/')) {
+        fs.mkdirSync('test/build/');
       }
-    );
-  }());
+    });
 
-  (function() {
-    var inputPath = 'test/resources/simple.scss';
-    var outputPath = 'test/build/simple.css';
+    (function() {
+      var inputPath = 'test/resources/simple.scss';
 
-    if (fs.existsSync(outputPath)) {
-      fs.unlinkSync(outputPath);
-    }
-
-    testCommandAsync(
-      clientPath + ' ' + inputPath + ' -o ' + outputPath,
-      "outputs contents of the input file to the output file",
-      function(subprocess, done) {
-        var interval = setInterval(function() {
-          if (fs.existsSync(outputPath)) {
-            assert.equal(
-              fs.readFileSync(inputPath).toString(),
-              fs.readFileSync(outputPath).toString()
-            );
-            clearInterval(interval);
+      testCommandAsync(
+        clientPath + ' ' + inputPath,
+        "output contents of the input file to stdout if there is no '-o' option",
+        function(subprocess, done) {
+          // file content should appear in the stdout
+          subprocess.stdout.on('data', function(data) {
+            assert.equal(data.toString(), fs.readFileSync(inputPath).toString());
+            subprocess.kill();
             done();
-          }
-        }, 20);
+          });
+        }
+      );
+    }());
+
+    (function() {
+      var inputPath = 'test/resources/simple.scss';
+      var outputPath = 'test/build/simple.css';
+
+      if (fs.existsSync(outputPath)) {
+        fs.unlinkSync(outputPath);
       }
-    );
-  }());
+
+      testCommandAsync(
+        clientPath + ' ' + inputPath + ' -o ' + outputPath,
+        "output contents of the input file to the output file",
+        function(subprocess, done) {
+          var interval = setInterval(function() {
+            if (fs.existsSync(outputPath)) {
+              assert.equal(
+                fs.readFileSync(inputPath).toString(),
+                fs.readFileSync(outputPath).toString()
+              );
+              clearInterval(interval);
+              done();
+            }
+          }, 20);
+        }
+      );
+    }());
+  });
 });
